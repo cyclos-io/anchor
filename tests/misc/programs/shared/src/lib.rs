b025ca045a3621bc9e884e3ceadc7604c2020492
Aadhinana <ajibaa@gmail.com>
2021-11-18 14:42:48 +0530

    WIP: Add failing test, fix IDL first

--- tests/misc/programs/shared/src/lib.rs
+++ tests/misc/programs/shared/src/lib.rs
@@ -1,7 +1,35 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+mod shared {
+    use super::*;
+    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
+        Ok(())
     }
+
+    pub fn test_vec_mut(ctx: Context<TestVecMut>, filterable: Pubkey) -> ProgramResult {
+        for acc in ctx.accounts.data.iter_mut() {
+            acc.filterable = filterable;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct TestVecMut<'info> {
+    #[account(mut)]
+    pub data: Vec<Account<'info, DataWithFilter>>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct DataWithFilter {
+    pub authority: Pubkey,
+    pub filterable: Pubkey,
 }
